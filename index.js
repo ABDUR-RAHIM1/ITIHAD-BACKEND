f96c819ef5978a07b166e5b4f877f535d4062c0b
AbdurRahim <89129980+ABDUR-RAHIM1@users.noreply.github.com>
2023-07-02 10:55:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const connectDB = require("./Database/db"); 
-const PORT = process.env.PORT;  
+const PORT = process.env.PORT || 8000;  
 
 app.listen(PORT, () => {
     console.log(`Server Is Running On Port : http://localhost:${PORT}`)
