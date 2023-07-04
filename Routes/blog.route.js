4791c20e01d1b836641de30781919b3dd6ce0c74
AbdurRahim <89129980+ABDUR-RAHIM1@users.noreply.github.com>
2023-07-04 16:53:52 +0600

    Update blog.route.js
    blog updated api raoutes added

--- Routes/blog.route.js
+++ Routes/blog.route.js
@@ -1,9 +1,10 @@
-const { getBlogs, addBlogs, deleteBlogs } = require("../Controllers/blog.controller")
+const { getBlogs, addBlogs, deleteBlogs, updateBlog } = require("../Controllers/blog.controller")
 
 const router = require("express").Router()
 
 router.get("/blogs", getBlogs)
 router.post("/blogs", addBlogs)
+router.put("/blog/:id", updateBlog)
 router.delete("/blogs/:id", deleteBlogs)
 
 module.exports  = router
