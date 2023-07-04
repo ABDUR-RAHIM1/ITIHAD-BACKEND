78a6d141a150df74e5286d8c935f740c8684737b
AbdurRahim <89129980+ABDUR-RAHIM1@users.noreply.github.com>
2023-07-04 16:54:40 +0600

    Update user.route.js
    updated user api addedd

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,10 +1,11 @@
-const { getuser, deleteUser, createUser, loginUser } = require("../Controllers/user.controller")
+const { getuser, deleteUser, createUser, loginUser, updateUser } = require("../Controllers/user.controller")
 
 const router = require("express").Router()
 
 router.get("/users",getuser)
 router.post("/create-user", createUser)
 router.post("/login-user", loginUser)
+router.put("/update-user/:id", updateUser)
 router.delete("/delete-user/:id", deleteUser)
 
 module.exports = router
