690f3378b8d58b8e5072ae6abcedf3e877e0d6ee
AbdurRahim <89129980+ABDUR-RAHIM1@users.noreply.github.com>
2023-07-04 16:53:11 +0600

    Update admin.route.js
    admin nupdated api routes addedd

--- Routes/admin.route.js
+++ Routes/admin.route.js
@@ -1,10 +1,11 @@
-const { getAdmin, loginAdmin, deleteAdmin, createModaretor } = require('../Controllers/admin.controller');
+const { getAdmin, loginAdmin, deleteAdmin, createModaretor, updateAdmin } = require('../Controllers/admin.controller');
 
 const router = require('express').Router()
 
 router.get("/admins", getAdmin)
 router.post("/modaretor-create", createModaretor)
 router.post("/admin-login", loginAdmin)
+router.put("/admin-update/:id", updateAdmin)
 router.delete("/admin-delete/:id", deleteAdmin)
 
 module.exports = router;
